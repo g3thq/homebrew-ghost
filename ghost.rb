7efc75c8b897abd10d478ae44f631095438f595a
Bryan Morrow <8430040+bryankmorrow@users.noreply.github.com>
2021-06-28 10:14:32 -0500

    v0.4.11

--- ghost.rb
+++ ghost.rb
@@ -4,24 +4,24 @@
 class Ghost < Formula
   desc ""
   homepage "https://github.com/g3thq/ghost"
-  version "0.4.10"
+  version "0.4.11"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/g3thq/ghost/releases/download/v0.4.10/ghost_0.4.10_MacOS-64bit.zip"
-    sha256 "cc2c63d9c8a5220da9cb04ed4af548f1f389c86888ef3a1e37d59f21c164c335"
+    url "https://github.com/g3thq/ghost/releases/download/v0.4.11/ghost_0.4.11_MacOS-64bit.zip"
+    sha256 "e7599c37b25745b6b2fc14a5fba936a415ae9cd7ee4e489384569e67794ef4ff"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/g3thq/ghost/releases/download/v0.4.10/ghost_0.4.10_MacOS-ARM64.zip"
-    sha256 "061c4aa6f2c7c3464b8970a3d17d0673accc5efb563a1729c5be279abc8b9c07"
+    url "https://github.com/g3thq/ghost/releases/download/v0.4.11/ghost_0.4.11_MacOS-ARM64.zip"
+    sha256 "a7c2be7a758316446b65220a0f385946651c2739ebf51ed921980333d689fc6e"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/g3thq/ghost/releases/download/v0.4.10/ghost_0.4.10_Linux-64bit.tar.gz"
-    sha256 "b773fbf8ebbccc15eeaeb2b442f2dd0cf61b09a00f3a2edf46b28f7ecd2c0ca2"
+    url "https://github.com/g3thq/ghost/releases/download/v0.4.11/ghost_0.4.11_Linux-64bit.tar.gz"
+    sha256 "53a2964de358676068e8fd482bf1218df62933b2af0fc187287a46f0b209ebab"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/g3thq/ghost/releases/download/v0.4.10/ghost_0.4.10_Linux-ARM64.tar.gz"
-    sha256 "71c64c7bad7b79e6a7a6b70e04b913ac792aefd15dd1520dd222b07e34a63ac6"
+    url "https://github.com/g3thq/ghost/releases/download/v0.4.11/ghost_0.4.11_Linux-ARM64.tar.gz"
+    sha256 "2147b1ce94c28ecfe2865bb51958ae3319a81e28d9f6bc187360a4bed98b6e07"
   end
 
   def install
