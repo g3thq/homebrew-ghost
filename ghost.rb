3904b5b6a1e01b79f8b10d8c8c7dd7f2f2659e5e
Bryan Morrow <bkmorrow@gmail.com>
2021-11-10 17:54:32 -0600

    Update ghost.rb

--- ghost.rb
+++ ghost.rb
@@ -9,7 +9,7 @@ class Ghost < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/g3thq/ghost/releases/download/v0.6.9/ghost_0.6.9_MacOS-64bit.zip"
-    sha256 "46087d35815ab96d11635d6ba7df239356a20b4fc8aa1cf6f69fd2870bad27ae"
+    sha256 "1ccda3d0f43c5bcddedfceedab2290b7dcb2b165f849330c93e3b962e851536c"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/g3thq/ghost/releases/download/v0.6.9/ghost_0.6.9_MacOS-ARM64.zip"
@@ -17,7 +17,7 @@ class Ghost < Formula
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/g3thq/ghost/releases/download/v0.6.9/ghost_0.6.9_Linux-64bit.tar.gz"
-    sha256 "1ccda3d0f43c5bcddedfceedab2290b7dcb2b165f849330c93e3b962e851536c"
+    sha256 "118ffe34e873af730ce81640d29006af64c0f05db052a40c1a9854e72cc55753"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/g3thq/ghost/releases/download/v0.6.9/ghost_0.6.9_Linux-ARM64.tar.gz"
