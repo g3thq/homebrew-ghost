526f3d29d70fb7508fa14dc806f44c09d1aa67fb
Bryan Morrow <bkmorrow@gmail.com>
2021-11-10 17:52:39 -0600

    v0.6.9

--- ghost.rb
+++ ghost.rb
@@ -4,24 +4,24 @@
 class Ghost < Formula
   desc "Cloud Native shift-left scanner for containers and infrastructure as code"
   homepage "https://github.com/g3thq/ghost"
-  version "0.5.1"
+  version "0.6.9"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/g3thq/ghost/releases/download/v0.5.1/ghost_0.5.1_MacOS-64bit.zip"
+    url "https://github.com/g3thq/ghost/releases/download/v0.6.9/ghost_0.6.9_MacOS-64bit.zip"
     sha256 "46087d35815ab96d11635d6ba7df239356a20b4fc8aa1cf6f69fd2870bad27ae"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/g3thq/ghost/releases/download/v0.5.1/ghost_0.5.1_MacOS-ARM64.zip"
-    sha256 "abdf61c514a4d882b03a35760c50256c5851e0784773a0aa196aafbd0897719b"
+    url "https://github.com/g3thq/ghost/releases/download/v0.6.9/ghost_0.6.9_MacOS-ARM64.zip"
+    sha256 "6bd3e522ee81c8098b08e65fd6c6849573420d7094dc9a8e68119e9145e442ff"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/g3thq/ghost/releases/download/v0.5.1/ghost_0.5.1_Linux-64bit.tar.gz"
-    sha256 "9fdf81f1d43085ea24a5abc364f0a42a6f0fb357d3e5e2b024d00f729ac13bf5"
+    url "https://github.com/g3thq/ghost/releases/download/v0.6.9/ghost_0.6.9_Linux-64bit.tar.gz"
+    sha256 "1ccda3d0f43c5bcddedfceedab2290b7dcb2b165f849330c93e3b962e851536c"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/g3thq/ghost/releases/download/v0.5.1/ghost_0.5.1_Linux-ARM64.tar.gz"
-    sha256 "37b7cd0215582caf66f337f70a2dc4cd41fac961fba12921a701735fd0c1e3c9"
+    url "https://github.com/g3thq/ghost/releases/download/v0.6.9/ghost_0.6.9_Linux-ARM64.tar.gz"
+    sha256 "3d0bf1421dfe097c820a4c8a9e4e1ea58a5fc12e1847b0e4bb13f29563819a19"
   end
 
   def install
